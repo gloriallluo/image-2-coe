b20ac9cd6d0ce9b1890bb6b8f01a0cceb31d2497
罗境佳 <2018013469@secoder.net>
2020-11-24 11:20:17 +0800

    change image2coe

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 image-2-coe
 ===========
 
-Python 2.7 script to convert an image to the Xilinx .coe format
+- Python 3.7 script to convert an image to the Xilinx .coe format.
+- Can also convert an image to risc-v code, saving the image into block ram.
 
 To Use
 -------
 Copy the image2coe.py file to the directory where your image resides.
 Open a terminal and change to the same directory. 
 Run the image2coe.py file by typing:
-`python image2coe.py`
+`python3 image2coe.py`
 You will be prompted for the name of your image. Enter it and hit the return key.
 
-When you make your BRAM in Xilinx select 'Single Port ROM' and enter '8' as the Read Width and the number that is returned in the terminal as Total addresses as the Read Depth.
+When you make your BRAM in Xilinx select 'Simple Dual RAM' and enter '8' as the Read Width and the number that is returned in the terminal as Total addresses as the Read Depth.
--- image2coe.py
+++ image2coe.py
@@ -2,7 +2,7 @@
 '''
 File Name: image2coe.py
 Author: Jesse Millwood
-Python Version: 2.7
+Python Version: 3.7
 Date: March 12 2014
 
 
@@ -28,8 +28,6 @@ import sys
 from PIL import Image 
 
 
-
-
 def Convert (ImageName):
 	"""
 		This converts the given image into a Xilinx Coefficients (.coe) file.
@@ -38,7 +36,7 @@ def Convert (ImageName):
 		or provide the absolute path. 
 	"""
 	# Open image
-	img 	= Image.open(ImageName)
+	img = Image.open(ImageName)
 	# Verify that the image is in the 'RGB' mode, every pixel is described by 
 	# three bytes
 	if img.mode != 'RGB':
@@ -54,11 +52,11 @@ def Convert (ImageName):
 	filetype = ImageName[ImageName.find('.'):]
 	filename = ImageName.replace(filetype,'.coe')
 	imgcoe	= open(filename,'wb')
-	imgcoe.write(';	VGA Memory Map\n')
-	imgcoe.write('; .COE file with hex coefficients\n')
-	imgcoe.write('; Height: {0}, Width: {1}\n'.format(height,width))
-	imgcoe.write('memory_initialization_radix = 16;\n')
-	imgcoe.write('memory_initialization_vector =\n')
+	imgcoe.write(b'; VGA Memory Map\n')
+	imgcoe.write(b'; .COE file with hex coefficients\n')
+	imgcoe.write(bytes(f'; Height: {height}, Width: {width}\n', encoding='utf8'))
+	imgcoe.write(b'memory_initialization_radix = 16;\n')
+	imgcoe.write(b'memory_initialization_vector =\n')
 	
 	# Iterate through every pixel, retain the 3 least significant bits for the
 	# red and green bytes and the 2 least significant bits for the blue byte. 
@@ -74,8 +72,8 @@ def Convert (ImageName):
 			try:
 				R,G,B = img.getpixel((c,r))
 			except IndexError:
-				print 'Index Error Occurred At:'
-				print 'c: {}, r:{}'.format(c,r)
+				print('Index Error Occurred At:')
+				print(f'c: {c}, r: {r}')
 				sys.exit()
 			# convert the value (0-255) to a binary string by cutting off the 
 			# '0b' part and left filling zeros until the string represents 8 bits
@@ -85,36 +83,35 @@ def Convert (ImageName):
 			Gb = bin(G)[2:].zfill(8)[:3]
 			Bb = bin(B)[2:].zfill(8)[:2]
 			
-			Outbyte = Rb+Gb+Bb
+			Outbyte = Rb + Gb + Bb
 			# Check for Value Error, happened when the case of the pixel being 
 			# zero was not handled properly	
 			try:
-				imgcoe.write('%2.2X'%int(Outbyte,2))
+				imgcoe.write(bytes('%2.2X' % int(Outbyte,2), encoding='utf8'))
 			except ValueError:
-				print 'Value Error Occurred At:'
-				print 'Contents of Outbyte: {0} at r:{1} c:{2}'.format(Outbyte,r,c)
-				print 'R:{0} G:{1} B{2}'.format(R,G,B)
-				print 'Rb:{0} Gb:{1} Bb:{2}'.format(Rb,Gb,Bb)
+				print('Value Error Occurred At:')
+				print(f'Contents of Outbyte: {Outbyte} at r:{r} c:{c}')
+				print(f'R: {R} G: {G} B: {B}')
+				print(f'Rb: {Rb} Gb: {Gb} Bb: {Bb}')
 				sys.exit()
 			# Write correct punctuation depending on line end, byte end,
 			# or file end
-			if c==width-1 and r==height-1:
-				imgcoe.write(';')
+			if c == width - 1 and r == height - 1:
+				imgcoe.write(b';')
 			else:
-				if cnt%32 == 0:
-					imgcoe.write(',\n')
-					line_cnt+=1
+				if cnt % 8 == 0:
+					imgcoe.write(b',\n')
+					line_cnt += 1
 				else:
-					imgcoe.write(',')
+					imgcoe.write(b',')
 	imgcoe.close()
-	print 'Xilinx Coefficients File:{} DONE'.format(filename)
-	print 'Converted from {} to .coe'.format(filetype)
-	print 'Size: h:{} pixels w:{} pixels'.format(height,width)
-	print 'COE file is 32 bits wide and {} bits deep'.format(line_cnt)
-	print 'Total addresses: {}'.format(32*(line_cnt+1))
-
+	print(f'Xilinx Coefficients File:{filename} DONE')
+	print(f'Converted from {filetype} to .coe')
+	print(f'Size: h:{height} pixels w:{width} pixels')
+	print(f'COE file is 8 bits wide and {line_cnt} bits deep')
+	print(f'Total addresses: {8 * (line_cnt + 1)}')
 
 
 if __name__ == '__main__':
-	ImageName = raw_input('Enter the name of your image: ')
+	ImageName = input('Enter the name of your image: ')
 	Convert(ImageName)
